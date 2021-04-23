596c89c69244b95028d605233c38429ada2c6289
Paul-Louis NECH <pln@algolia.com>
2021-04-23 19:27:28 +0200

    Gatsby: save wip

--- pln/src/pages/hydra.js
+++ pln/src/pages/hydra.js
@@ -4,18 +4,23 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import HydraSynth from "../components/hydra-synth"
+
 
 const Hydra = () => (
   <Layout>
     <Seo title="Hydra" />
+    <HydraSynth canvasId="myCanvas"/>
     <h1>Hydra</h1>
     <p>With <a href="https://github.com/ojack/hydra/">Hydra</a>, I create some eye-catchers:</p>
+    <canvas id="canvas"/>
+    <h2>Work Process</h2>
     <StaticImage
       src="../images/WIP_GaLactic.png"
       width={2048}
       quality={95}
       formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
+      alt="A screenshot of four intermediary steps of the GaLactic work"
       style={{ marginBottom: `1.45rem` }}
     />
     <Link to="/">Home</Link>
