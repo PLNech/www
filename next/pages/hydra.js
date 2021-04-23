3b9383ad403e0104bd9cd4101c5d0ef2afc155ed
Paul-Louis NECH <pln@algolia.com>
2021-04-23 22:39:49 +0200

    fix: Hydra

--- next/pages/hydra.js
+++ next/pages/hydra.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Head from "next/head";
+import Layout from "../components/layout";
 
 export async function getStaticProps(context) {
   return {
@@ -10,19 +11,22 @@ export async function getStaticProps(context) {
 
 export default function Hydra() {
   return (
+    <>
     <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>PLN's Hydra works</title>
     </Head>
-    <div>
-      <h1>I create visuals with patterns</h1>
-      <Image
-        alt="Mountains"
-        src="/images/WIP_GaLactic.png"
-        layout="fill"
-        // width={700}
-        // height={475}
-      />
-    </div>
+    <Layout>
+      <div>
+        <h1>I create visuals with patterns</h1>
+        <Image
+          alt="Mountains"
+          src="/images/WIP_GaLactic.png"
+          layout="fill"
+          // width={700}
+          // height={475}
+        />
+      </div>
+      </Layout>
   );
 }
